1b4eb1b6277956f3efee004b09fab566297c6656
yang <b05203017@ntu.edu.tw>
2019-08-02 15:54:08 +0800

    reset password done

--- dream-of-the-red-chamber/settings.py
+++ dream-of-the-red-chamber/settings.py
@@ -136,3 +136,11 @@ STATIC_URL = '/static/'
 # djauth/settings.py
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'peekdogs.ntu@gmail.com'
+EMAIL_HOST_PASSWORD = 'peekdogs'
