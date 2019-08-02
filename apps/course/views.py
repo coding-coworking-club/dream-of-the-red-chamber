4b2c371982ce093901affdab9b487382161b54c7
kemerical <48002396+kemerical@users.noreply.github.com>
2019-08-02 18:41:52 +0800

    add home-page view

--- apps/course/views.py
+++ apps/course/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import Post
 
 # Create your views here.
+def course_homepage(request):
+	keyword = request.GET.get('keyword', '')
+	course_list = Post.objects.filter(syllabus__contains=keyword)
+	template_name='course/course_homepage.html'
+	return render(request, template_name)
 
 def course(request):
 	keyword = request.GET.get('keyword', '')
