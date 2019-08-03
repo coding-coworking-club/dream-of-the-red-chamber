392af85614217f6a653a95958828668158909b7f
YeuTong <yeutong00@gmail.com>
2019-08-03 21:59:32 +0800

    history and redirect

--- apps/course/views.py
+++ apps/course/views.py
@@ -1,19 +1,41 @@
 from django.shortcuts import render
 from .models import Post
+from datetime import datetime
+from apps.history.models import History
+from django.http import HttpRequest
+#from django.db.models import Count
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
 
 def course(request):
 	keyword = request.GET.get('keyword', '')
-	course_list = Post.objects.filter(syllabus__contains=keyword)
+	if keyword == "":
+		course_list = []
+	else:
+		course_list = Post.objects.filter(syllabus__contains=keyword)
 	template_name = 'course/course.html'
-	context = {'course_list': course_list}
+	context = {'course_list': course_list,'keyword': keyword}
 	return render(request, template_name, context)
 
 def course_detail(request, course_id):
 	course = Post.objects.get(id=course_id)
-	template_name = 'course/course_detail.html'
-	context = {'course':course}
+	keyword = request.GET.get('keyword', '')
+	time = datetime.now()
+	
+	#template_name = 'course/course_detail.html'
+	#context = {'course':course}
+	#return render(request, template_name, context)
+	
+	if not request.user.is_authenticated:
+		History.objects.create(course=course,keyword=keyword,time=time)
+	else:
+		History.objects.create(user=request.user,course=course,keyword=keyword,time=time)
+	
 	course.click_times += 1
 	course.save()
-	return render(request, template_name, context)
+	
+	#hot_keyword = History.objects.values('keyword').annotate(c=Count('keyword')).order_by('-c')
+	
+	return HttpResponseRedirect(course.class_url)
+	
